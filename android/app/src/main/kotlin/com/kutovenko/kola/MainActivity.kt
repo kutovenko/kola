7a2e8c8772a59ad257776f98ea99d800f5b0dee9
Aliaksei <aliaksei.kutavenka@aventus.work>
2021-07-15 22:45:30 +0300

    Initial commit. Project structure

---
+++ android/app/src/main/kotlin/com/kutovenko/kola/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.kutovenko.kola
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/feature/start_screen/search_screen.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class SearchScreen extends StatelessWidget {
+  const SearchScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+        floatingActionButton: FloatingActionButton(
+          backgroundColor: Colors.red,
+          onPressed: () {},
+          child: const Icon(
+            Icons.play_arrow,
+            color: Colors.white,
+          ),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: 0, // this will be set when a new tab is tapped
+          items: [
+            BottomNavigationBarItem(
+              icon: const Icon(Icons.search),
+              label: 'Поиск',
+            ),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.favorite), title: Text('Фавориты'))
+          ],
+        ),
+      ),
+    );
+  }
+}
