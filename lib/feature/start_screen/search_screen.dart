c640f347abd5ebb8cd97fcebd8c7a25d0be3e67e
Aliaksei <aliaksei.kutavenka@aventus.work>
2021-07-19 11:21:08 +0300

    Color search widget created. Custom ColorPicker widget added

--- lib/feature/start_screen/search_screen.dart
+++ lib/feature/start_screen/search_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:kola/core/utils/color_picker.dart';
 
 class SearchScreen extends StatelessWidget {
   const SearchScreen({Key? key}) : super(key: key);
@@ -7,6 +8,9 @@ class SearchScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
+        body: SingleChildScrollView(
+          child: ColorSearchWidget(),
+        ),
         floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
         floatingActionButton: FloatingActionButton(
           backgroundColor: Colors.red,
@@ -28,6 +32,59 @@ class SearchScreen extends StatelessWidget {
           ],
         ),
       ),
+    );
+  }
+}
+
+class ColorSearchWidget extends StatefulWidget {
+  const ColorSearchWidget({Key? key}) : super(key: key);
+
+  @override
+  _ColorSearchWidgetState createState() => _ColorSearchWidgetState();
+}
+
+class _ColorSearchWidgetState extends State<ColorSearchWidget> {
+  Color pickerColor = Color(0xFFFFFFFF);
+  Color? currentColor;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Text(
+            'Поиск по цвету. ВЫберите основной и вспомогательный цвет интерьера'),
+        MaterialPicker(
+          pickerColor: pickerColor,
+          onColorChanged: (color) => {pickerColor = color},
+        ),
+        Row(
+          children: [
+            TextButton(
+              style: ButtonStyle(
+                  backgroundColor:
+                      MaterialStateProperty.all(currentColor ?? Colors.white)),
+              onPressed: () {
+                setState(() {
+                  currentColor = pickerColor;
+                });
+              },
+              child: Text('Выбрать'),
+            ),
+            TextButton(
+              style: ButtonStyle(
+                  backgroundColor:
+                      MaterialStateProperty.all(currentColor ?? Colors.white)),
+              onPressed: () {
+                setState(() {
+                  currentColor = pickerColor;
+                });
+              },
+              child: Text('Choose'),
+            ),
+          ],
+        )
+      ],
     );
   }
 }
--- lib/mock/mock_data.dart
+++ lib/mock/mock_data.dart
@@ -1,10 +1,48 @@
+import 'package:flutter/material.dart';
 import 'package:kola/core/models/location_entity.dart';
 
 class MockData {
-  LocationEntity greenCafe = LocationEntity(
+  LocationEntity brownCafe = LocationEntity(
       id: '1',
-      locationName: 'Red Cafe',
+      locationName: 'Brown Cafe',
       locationType: LocationType.CAFE,
       address: 'ul. Berezovaya Roscha 99',
-      colors: ColorDescription());
+      colors: ColorDescription(
+          primary: ColorEntity(
+              colorType: ColorType.PRIMARY, value: Color(0xFF665e49)),
+          secondary: ColorEntity(
+              colorType: ColorType.SECONDARY, value: Color(0xFFf9ff85))));
+
+  LocationEntity greenCafe = LocationEntity(
+      id: '2',
+      locationName: 'Green Cafe',
+      locationType: LocationType.CAFE,
+      address: 'ul. Hamam 99',
+      colors: ColorDescription(
+          primary: ColorEntity(
+              colorType: ColorType.PRIMARY, value: Color(0xFF5c9c52)),
+          secondary: ColorEntity(
+              colorType: ColorType.SECONDARY, value: Color(0xFFededed))));
+
+  LocationEntity pinkCafe = LocationEntity(
+      id: '3',
+      locationName: 'Pink Cafe',
+      locationType: LocationType.CAFE,
+      address: 'ul. Krasnaya 99',
+      colors: ColorDescription(
+          primary: ColorEntity(
+              colorType: ColorType.PRIMARY, value: Color(0xFFe67777)),
+          secondary: ColorEntity(
+              colorType: ColorType.SECONDARY, value: Color(0xFFf9ff85))));
+
+  LocationEntity whiteCafe = LocationEntity(
+      id: '4',
+      locationName: 'White Cafe',
+      locationType: LocationType.CAFE,
+      address: 'ul. Belaya 99',
+      colors: ColorDescription(
+          primary: ColorEntity(
+              colorType: ColorType.PRIMARY, value: Color(0xFFe8e8e8)),
+          secondary: ColorEntity(
+              colorType: ColorType.SECONDARY, value: Color(0xFF121111))));
 }
